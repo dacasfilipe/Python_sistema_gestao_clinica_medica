1854abf469dd1a732712bc0a970258847a4ef0cb
dacasfilipe <filiperdacas@gmail.com>
2023-05-14 22:49:21 -0300

    inserido valores para ajudar nos agendamentos

--- main.py
+++ main.py
@@ -34,4 +34,13 @@ class Clinica:
         self.nomes_pacientes = {}
         self.nomes_medicos = {}
         self.nomes_planos = ['SUS', 'UNIMED', 'CAUZZO']
+        #aqui embaixo valores para os agendamentos
+        self.agenda = {}
+        self.dias_agendamento = ['SEGUNDA', 'QUARTA', 'SEXTA']
+        self.horarios = ['08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00']
+        self.valor_consulta = 250
+        self.num_consultas = 0
+        self.receitas_consultas = 0
+        self.rel_agendamento = []
+        self.hist_receita_consultas = {}
         
