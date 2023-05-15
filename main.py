878bd5261752ac1a7d4b104e1ad004c58943ad69
dacasfilipe <filiperdacas@gmail.com>
2023-05-14 22:43:23 -0300

    criação de classe Clinica e objetos no main.py

--- main.py
+++ main.py
@@ -8,3 +8,29 @@ import sqlite3
 from bases import cria_bases
 cria_bases()
 
+class Clinica:
+    def __init__(self):
+        self.nome_paciente = ''
+        self.identidade = ''
+        self.profissao = ''
+        self.nome_medico = ''
+        self.nome_plano = ''
+        self.crm = ''
+        self.num_credencial = ''
+        self.especialidade = ''
+        self.fone = ''
+        self.telefone = ''
+        self.email = ''
+        self.endereco = ''
+        self.data_nascimento = ''
+        self.convenio = ''
+        self.outros = ''
+        self.plano = ''
+        self.data = ''
+        self.hora = ''
+        self.prontuario = {}
+        self.base_medicos = {}
+        self.nomes_pacientes = {}
+        self.nomes_medicos = {}
+        self.nomes_planos = ['SUS', 'UNIMED', 'CAUZZO']
+        
