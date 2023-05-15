13b2b85bbbbe93bd441cd7dfbdcbc7f38088ce11
dacasfilipe <filiperdacas@gmail.com>
2023-05-14 22:33:10 -0300

    adicionado ao main.py a importação das bases de dados.

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ from operator import getitem
 from datetime import date
 from calendar import monthrange
 import sqlite3
+
+#importar a função para criar as bases de dados
+from bases import cria_bases
+cria_bases()
+
