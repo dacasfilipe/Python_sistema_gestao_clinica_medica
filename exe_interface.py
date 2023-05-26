b2ae99385d58bc3a755ab644e3dc33e0fe0132c3
dacasfilipe <filiperdacas@gmail.com>
2023-05-26 20:13:47 -0300

    criado menu de navegação para as páginas do sistema

--- exe_interface.py
+++ exe_interface.py
@@ -10,3 +10,15 @@ from app import Clinica
 
 sistema = Clinica()
 
+st.sidebar.title('Menu Principal')
+
+menu = st.sidebar.selectbox('Selecione uma opção', ['Página Principal',
+                                                    'Agendar Consulta',
+                                                    'Cadastrar Paciente',
+                                                    'Cadastrar Médico',
+                                                    'Exibir Prontuários'])
+
+if menu == 'Página Principal':
+    st.title('Sistema Clínica')
+    st.text('Agenda')
+    st.write(sistema.agenda)
