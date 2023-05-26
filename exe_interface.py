4bddc9512f3934fe1f6e200da8cdf0731681a8d7
dacasfilipe <filiperdacas@gmail.com>
2023-05-26 20:09:02 -0300

    implementado o arquivo exe_interface

---
+++ exe_interface.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import sqlite3
+from calendar import monthrange
+from datetime import date
+from operator import getitem
+import streamlit as st 
+import datetime
+
+from app import Clinica
+
+sistema = Clinica()
+
