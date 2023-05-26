1745bae015c52d3dbb2709bf01abce11e3d07885
dacasfilipe <filiperdacas@gmail.com>
2023-05-26 20:28:12 -0300

    criado formulário para cadastra paciente no streamlit

--- exe_interface.py
+++ exe_interface.py
@@ -22,3 +22,17 @@ if menu == 'Página Principal':
     st.title('Sistema Clínica')
     st.text('Agenda')
     st.write(sistema.agenda)
+
+elif menu == 'Cadastrar Paciente':
+    with st.form(key = 'cadastro_paciente'):
+        st.title('Cadastro de Pacientes')
+        nome = st.text_input('Digite o nome completo do paciente: ').upper()
+        identidade = st.text_input('Digite o número da identidade: ')
+        data_nasc = st.text_input('Digite a data de nascimento: ')
+        fone = st.text_input('Digite o telefone: ')
+        email = st.text_input('Digite o email:').upper()
+        endereco = st.text_input('Digite o endereço: ').upper()
+        profissao = st.text_input('Digite a atividade profissional: ').upper()
+        outros = st.text_input('Digite outras informações: ').upper()
+        botao_enviar = st.form_submit_button('Enviar')
+    
