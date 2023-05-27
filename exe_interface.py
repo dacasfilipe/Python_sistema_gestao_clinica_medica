7ab9efe6cb11bb50173cd4047e5a53a8cbafb46f
dacasfilipe <filiperdacas@gmail.com>
2023-05-27 17:32:04 -0300

    atualizado a base de dados de nomes de medicos

--- exe_interface.py
+++ exe_interface.py
@@ -3,7 +3,7 @@ import sqlite3
 from calendar import monthrange
 from datetime import date
 from operator import getitem
-import streamlit as st 
+import streamlit as st
 import datetime
 
 from app import Clinica
@@ -24,7 +24,7 @@ if menu == 'Página Principal':
     st.write(sistema.agenda)
 
 elif menu == 'Cadastrar Paciente':
-    with st.form(key = 'cadastro_paciente'):
+    with st.form(key='cadastro_paciente'):
         st.title('Cadastro de Pacientes')
         nome = st.text_input('Digite o nome completo do paciente: ').upper()
         identidade = st.text_input('Digite o número da identidade: ')
@@ -44,57 +44,56 @@ elif menu == 'Cadastrar Paciente':
         sistema.endereco = endereco
         sistema.profissao = profissao
         sistema.outros = outros
-    
+
 conexao = sqlite3.connect('base_pacientes.db')
 cursor_base = conexao.cursor(conexao)
 cursor_base.execute("REPLACE INTO pacientes (nome_paciente, identidade, fone, email, endereco, data_nascimento, profissao, observacoes) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-                    {'nome_pac' : sistema.nome_paciente,
-                     'identidade' : sistema.identidade,
-                     'fone' : sistema.fone,
+                    {'nome_pac': sistema.nome_paciente,
+                     'identidade': sistema.identidade,
+                     'fone': sistema.fone,
                      'email': sistema.email,
-                     'endereco' : sistema.endereco,
-                     'data_nasc' : sistema.data_nasc,
-                     'profissao' : sistema.profissao,
-                     'observacoes' : sistema.outros})
+                     'endereco': sistema.endereco,
+                     'data_nasc': sistema.data_nasc,
+                     'profissao': sistema.profissao,
+                     'observacoes': sistema.outros})
 
 sistema.nomes_pacientes.append(sistema.nome_paciente)
 
 sistema.prontuario.update({
-        sistema.nome_paciente: {
-            'Nome': sistema.nome_paciente,
+    sistema.nome_paciente: {
+        'Nome': sistema.nome_paciente,
             'Data de Nascimento: ': sistema.data_nasc,
             'Telefone para contato: ': sistema.fone,
             'Endereço Residencial: ': sistema.endereco,
             'Observações: ': sistema.outros,
             'Número de Consultas: ': sistema.num_consultas,
             'Prontuário: ': sistema.rel_agendamento}
-    })
+})
 st.text('Cadastro Realizado / alterado com sucesso!')
 
 elif menu == 'Cadastrar Médico':
-    with st.form(key = 'cadastro_medico'):
-        st.title('Cadastro de Médico')
-        nome = st.text_input('Digite o nome completo do médico: ').upper()
-        crm = st.text_input('Digite o número do CRM: ')
-        especialidade = st.text_input('Digite a especialidade: ').upper()
-        botao_enviar = st.form_submit_button('Enviar')
+    with st.form(key='cadastro_medico'):
+            st.title('Cadastro de Médico')
+            nome = st.text_input('Digite o nome completo do médico: ').upper()
+            crm = st.text_input('Digite o número do CRM: ')
+            especialidade = st.text_input('Digite a especialidade: ').upper()
+            botao_enviar = st.form_submit_button('Enviar')
     if botao_enviar:
         sistema.nome_med = nome
         sistema.crm = crm
         sistema.especialidade = especialidade
-        
-        conexao = sqlite3.connect('base_medicos.db')
-        cursor_base = conexao.cursor()
-        cursor_base.execute("INSERT INTO medicos (nome_med, crm, especialidade) VALUES (?, ?, ?)",{
-            'nome_med' : sistema.nome_med,
-            'crm' : sistema.crm,
-            'especialidade' : sistema.especialidade
-        })
-        conexao.commit()
-        conexao.close()
 
-        sistema.nomes_medicos.append(sistema.nome_med)
-        sistema.base_medicos.update({f'{sistema.especialidade}'})
-        
-        st.text('Cadastro Realizado com sucesso!')
-    
+    conexao = sqlite3.connect('base_medicos.db')
+    cursor_base = conexao.cursor()
+    cursor_base.execute("INSERT INTO medicos (nome_med, crm, especialidade) VALUES (?, ?, ?)", {
+    'nome_med': sistema.nome_med,
+    'crm': sistema.crm,
+    'especialidade': sistema.especialidade
+    })
+    conexao.commit()
+    conexao.close()
+
+    sistema.nomes_medicos.append(sistema.nome_med)
+    sistema.base_medicos.update({f'{sistema.especialidade}'})
+
+    st.text('Cadastro Realizado com sucesso!')
